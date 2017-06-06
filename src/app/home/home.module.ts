29e68b1c1d31a18b2a639f68d1fdef5677a04c82
wuchunlin <1336649350@qq.com>
2017-06-06 17:03:52 +0800

    首页bs2

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -3,12 +3,14 @@ import { RouterModule } from '@angular/router';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HomeComponent } from "./home.component";
+import { CarouselModule } from 'ngx-bootstrap';
 
 @NgModule({
   imports: [
     CommonModule,
     RouterModule,
-    RouterModule.forChild(homeRoutes)
+    RouterModule.forChild(homeRoutes),
+    CarouselModule.forRoot()
   ],
   declarations: [
     HomeComponent
